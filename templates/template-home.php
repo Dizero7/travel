ad33587dc78546258c59688dfed0ae7d4916bf07
Dizero7 <diothers0@gmail.com>
2024-02-20 17:57:12 +0200

    hero, about, beauty

--- templates/template-home.php
+++ templates/template-home.php
@@ -28,30 +28,66 @@ get_header();
 			</div>
 			<div class="hero__locations">
 				<?php
-				$args = array(
-					'post_type' => 'hero_location',
-					'posts_per_page' => 4,
+				$locations = get_terms([
+						'taxonomy' => 'locations',
+						'hide_empty' => false,
+				]);
+
+				foreach ( $locations as $location ) {
+						$location_img = get_field( 'location_image', 'locations_' . $location->term_id );
+						$location_link = get_term_link( $location );
+						?>
+						<a href="<?= esc_url( $location_link ); ?>" class="hero__location">
+								<img src="<?= esc_url( $location_img['url'] ); ?>" alt="" class="location__image"/>
+								<h3 class="location__title"><?php echo esc_html( $term->name ); ?></h3>
+								<div class="location__shadow"></div>
+						</a>
+				<?php } ?>
+			</div>
+
+		</div>
+	</section>
+	<section class="about">
+		<div class="container">
+			<h2 class="about__heading">
+				<?php echo wp_kses_post ( get_field('about_heading') ); ?>
+			</h2>
+			<p class="about__text">
+				<?php echo esc_html ( get_field('about_text') ); ?>
+			</p>
+			<a href="<?php
+				$about_link = get_field('about_link');
+				esc_url($about_link['url'])
+				?>" class="button"><?php
+				echo esc_html($about_link['title'])
+				?>
+			</a>
+		</div>
+	</section>
+	<section class="beauty">
+		<div class="container">
+			<h2 class="beauty__heading">
+			<?php echo wp_kses_post ( get_field('beauty_heading') ); ?>
+			</h2>
+			<div class="beauty__locations">
+				<?php $args = [
+					'post_type' => 'beauty_place',
+					'posts_per_page' => 3,
 					'order' => 'ASC'
-				);
-				$posts = get_posts($args);
-				foreach ($posts as $post) {
+				];
+
+				$places = get_posts($args);
+
+				foreach ($places as $place) :
+				$place_link = get_field('place_link', $place->ID);
+				$place_img = get_field('place_image', $place->ID);
 				?>
-				<a href="<?php
-					$location_link = get_field('location_link');
-					echo esc_url($location_link['url']);
-					?>" class="location">
-					<img src="<?php
-					$location_img = get_field('location_image');
-					echo esc_url($location_img['url']); 
-					?>" alt="" class="location__image"/>
-					<h3 class="location__title"><?php 
-					echo esc_html($location_link['title']);
-					?></h3>
-					<div class="location__shadow"></div>
+				<a href="<?= esc_url($place_link['url']); ?>" class="beauty__place">
+					<img src="<?= esc_url($place_img['url']); ?>" alt="" class="place__image"/>
+					<h3 class="place__name"><?= esc_html(get_field('place_name', $place->ID));?></h3>
+					<h3 class="place__location"><?= esc_html(get_field('place_location', $place->ID));?></h3>
 				</a>
-				<?php
-				}
-				?>
+				<?php endforeach; ?>
 			</div>
 		</div>
 	</section>
